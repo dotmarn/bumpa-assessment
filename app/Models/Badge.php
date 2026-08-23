2896b25a88071306de099d504bc019deb2c8c31d
Kasim Ridwan <kasimsheyi20@gmail.com>
2026-08-23 09:45:55 +0100

    chore: added relationship

--- app/Models/Badge.php
+++ app/Models/Badge.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Attributes\Guarded;
 use Illuminate\Database\Eloquent\Attributes\UseFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Support\Carbon;
 
 /**
@@ -30,5 +32,20 @@ class Badge extends Model
             'required_achievements' => 'integer',
             'sort_order' => 'integer',
         ];
+    }
+
+    public function unlocks(): HasMany
+    {
+        return $this->hasMany(UserBadge::class);
+    }
+
+    public function users(): BelongsToMany
+    {
+        return $this->belongsToMany(
+            User::class,
+            'user_badges',
+        )
+            ->withPivot('unlocked_at')
+            ->withTimestamps();
     }
 }
